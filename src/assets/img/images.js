2b3919b83fff8a5942437b88d176b4a9cef2fa70
Ana Juarez <manitacoqui@gmail.com>
2024-03-11 10:09:24 -0300

    Add new images

--- src/assets/img/images.js
+++ src/assets/img/images.js
@@ -1,7 +1,70 @@
-import ryder from './ryder.png'
-import gun from './gun.png'
+/* characters */
+import chRyder from './characters/ch_ryder.png'
+import chBalla from './characters/ch_balla.png'
+import chBigSmoke from './characters/ch_bigSmoke.png'
+import chCesar from './characters/ch_cesar.png'
+import chCJ from './characters/ch_cj.png'
+import chDenisse from './characters/ch_denisse.png'
+import chHans from './characters/ch_hans.png'
+import chKen from './characters/ch_ken.png'
+import chMaccer from './characters/ch_maccer.png'
+import chMike from './characters/ch_mike.png'
+import chOgLoc from './characters/ch_ogLoc.png'
+import chPoli1 from './characters/ch_poli1.png'
+import chPoli2 from './characters/ch_poli2.png'
+import chSweet from './characters/ch_sweet.png'
+import chVago from './characters/ch_vago.png'
+
+/* badges */
+import bdPistola from './badges/bd_pistola.png'
+import bdAK from './badges/bd_ak47.png'
+import bdBazooka from './badges/bd_bazooka.png'
+import bdEscopeta from './badges/bd_escopeta.png'
+import bdFranco from './badges/bd_franco.png'
+import bdGolf from './badges/bd_golf.png'
+import bdKnife from './badges/bd_knife.png'
+import bdM4 from './badges/bd_m4.png'
+import bdMinigun from './badges/bd_minigun.png'
+import bdMotosierra from './badges/bd_motosierra.png'
+import bdPuño from './badges/bd_puño.png'
+import bdRecortada from './badges/bd_recortada.png'
+import bdSilenciador from './badges/bd_silenciador.png'
+import bdFuego from './badges/bd_fuego.png'
+import bdMetralleta from './badges/bd_metralleta.png'
+
 
 export default {
-    "img1": ryder,
-    "gun": gun
+    /* characters */
+    "ch_ryder": chRyder,
+    "ch_balla": chBalla,
+    "ch_bigSmoke": chBigSmoke,
+    "ch_cesar": chCesar,
+    "ch_cj": chCJ,
+    "ch_denisse": chDenisse,
+    "ch_hans": chHans,
+    "ch_ken": chKen,
+    "ch_maccer": chMaccer,
+    "ch_mike": chMike,
+    "ch_ogLoc": chOgLoc,
+    "ch_poli1": chPoli1,
+    "ch_poli2": chPoli2,
+    "ch_sweet": chSweet,
+    "ch_vago": chVago, 
+
+    /* badges */
+    "bd_pistola": bdPistola, 
+    "bd_AK47": bdAK, 
+    "bd_bazooka": bdBazooka, 
+    "bd_escopeta": bdEscopeta, 
+    "bd_franco": bdFranco, 
+    "bd_fuego": bdFuego, 
+    "bd_golf": bdGolf, 
+    "bd_knife": bdKnife, 
+    "bd_M4": bdM4, 
+    "bd_metralleta": bdMetralleta, 
+    "bd_minigun": bdMinigun, 
+    "bd_motosierra": bdMotosierra, 
+    "bd_puño": bdPuño, 
+    "bd_recortada": bdRecortada, 
+    "bd_silenciador": bdSilenciador
 }
